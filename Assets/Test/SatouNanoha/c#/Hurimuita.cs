4041f9a53ff55ff7b70ae5a19271147058312047
SatouNanoha <c612101309@tokushima-u.ac.jp>
2021-08-01 14:01:16 +0900

    Mainの時間表示を行いました。 先生の振り向きによるシーン移動を可能にしました。 （小林）

---
+++ Assets/Test/SatouNanoha/c#/Hurimuita.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hurimuita : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("DelayMethod", 2);  //2•bŒã‚ÉDelayMethod‚Ö
+    }
+
+    void DelayMethod()
+    {
+        SceneManager.LoadScene("Result2");
+    }
+}
---
+++ Assets/Test/SatouNanoha/c#/Timecount.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timecount : MonoBehaviour
+{
+    public GameObject main;
+    private Main mainCs;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = GameObject.Find("Main");
+        mainCs = main.GetComponent<Main>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeText.text = "ƒ^ƒCƒ€:" + mainCs.time.ToString();
+    }
+}
