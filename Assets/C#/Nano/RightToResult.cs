e42086fb70b273bd408fa59aa9582a8c7c71192a
hirokazu1108 <k.1108.hiro@gmail.com>
2021-08-08 05:59:33 +0900

    悪事画面で見つかった時の音を設定しました。

--- Assets/C#/Nano/RightToResult.cs
+++ Assets/C#/Nano/RightToResult.cs
@@ -6,21 +6,33 @@ using UnityEngine.SceneManagement;
 public class RightToResult : MonoBehaviour
 {
     public float RightTime; //Right‚É—ˆ‚Ä‚©‚ç‚ÌŠÔ
-    public float backTime;  //U‚èŒü‚­ŠÔ
+    public float alertTime;  //U‚èŒü‚­ŠÔ
+    bool alertCalledOnce = false; //U‚èŒü‚«‰¹‚ğˆê‰ñ‚¾‚¯–Â‚ç‚·
+
+    public AudioClip sound1; //ƒ^ƒCƒ€ƒAƒbƒv‰¹Œ¹
+    AudioSource audiosource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audiosource = GetComponent<AudioSource>(); //Component‚Ìæ“¾
     }
 
     // Update is called once per frame
     void Update()
     {
         RightTime = RightTime + Time.deltaTime;
-        backTime = Random.Range(8.0f, 15.0f);
-        if (RightTime > backTime)
+        alertTime = Random.Range(8.0f, 15.0f);
+        if (RightTime > alertTime && alertCalledOnce == false)
         {
-            SceneManager.LoadScene("Hurimuita");
+            audiosource.PlayOneShot(sound1); //Œx‰¹‚ğ–Â‚ç‚·
+            alertCalledOnce = true;
+            Invoke("ToHurimuitaMethod", 2); //Œx‚Ì2•bŒãHurimuita‚Ö
         }
+    }
+
+    void ToHurimuitaMethod()
+    {
+        SceneManager.LoadScene("Hurimuita");
     }
 }
--- Assets/C#/Nano/ShootingToResult.cs
+++ Assets/C#/Nano/ShootingToResult.cs
@@ -6,21 +6,33 @@ using UnityEngine.SceneManagement;
 public class ShootingToResult : MonoBehaviour
 {
     public float ShootingTime; //Shooting‚É—ˆ‚Ä‚©‚ç‚ÌŠÔ
-    public float backTime;  //U‚èŒü‚­ŠÔ
+    public float alertTime;  //U‚èŒü‚­ŠÔ
+    bool alertCalledOnce = false; //U‚èŒü‚«‰¹‚ğˆê‰ñ‚¾‚¯–Â‚ç‚·
+
+    public AudioClip sound1; //ƒ^ƒCƒ€ƒAƒbƒv‰¹Œ¹
+    AudioSource audiosource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audiosource = GetComponent<AudioSource>(); //Component‚Ìæ“¾
     }
 
     // Update is called once per frame
     void Update()
     {
         ShootingTime = ShootingTime + Time.deltaTime;
-        backTime = Random.Range(5.0f, 15.0f);
-        if (ShootingTime > backTime)
+        alertTime = Random.Range(5.0f, 15.0f);
+        if (ShootingTime > alertTime && alertCalledOnce == false)
         {
-            SceneManager.LoadScene("Hurimuita");
+            audiosource.PlayOneShot(sound1); //Œx‰¹‚ğ–Â‚ç‚·
+            alertCalledOnce = true;
+            Invoke("ToHurimuitaMethod", 2); //Œx‚Ì2•bŒãHurimuita‚Ö
         }
+    }
+    
+    void ToHurimuitaMethod()
+    {
+        SceneManager.LoadScene("Hurimuita");
     }
 }
