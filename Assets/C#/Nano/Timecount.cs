86e1eb498967e0a6da14ad2679be97f23ab69318
SatouNanoha <c612101309@tokushima-u.ac.jp>
2021-08-10 14:06:43 +0900

    全てのシーンでスコアが表示されるようにしました

--- Assets/C#/Nano/Timecount.cs
+++ Assets/C#/Nano/Timecount.cs
@@ -19,6 +19,6 @@ public class Timecount : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeText.text = "ƒ^ƒCƒ€:" + mainCs.time.ToString("f1");
+        timeText.text = "Žc‚èŽžŠÔ:" + mainCs.time.ToString("f1");
     }
 }
