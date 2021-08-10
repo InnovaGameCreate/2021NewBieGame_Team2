bb23e477188cda25edd300d9daaa6025c66688e8
hirokazu1108 <k.1108.hiro@gmail.com>
2021-08-10 18:36:54 +0900

    ScoreBoardの追加・編集

---
+++ Assets/C#/Hirokazu/ScoreBoard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    private GameObject main;
+    private Main mainCs;
+    public Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = GameObject.Find("Main");
+        mainCs = main.GetComponent<Main>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "SCORE" + mainCs.score.ToString("f1");
+    }
+}
