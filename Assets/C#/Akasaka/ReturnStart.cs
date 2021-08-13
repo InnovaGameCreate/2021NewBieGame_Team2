e308ac2d07c397a728315aac768e072cb06fb89d
kozaimikuto <c612101286@tokushima-u.ac.jp>
2021-08-13 17:45:24 +0900

    ボタンを治しました

---
+++ Assets/C#/Akasaka/ReturnStart.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void OnClick()
+    {
+        SceneManager.LoadScene("Starting");
+    }
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
